ca12babf3c9419f12742a9e2615b4dae688aa4cd
Dale Auten <dauten96@gmail.com>
2019-04-20 20:32:25 -0500

    a few techniques to detect text

--- read.py
+++ read.py
@@ -16,12 +16,6 @@ plt.show()
 
 '''
 
-
-
-
-
-
-
 from imutils.video import VideoStream
 import argparse
 import imutils
@@ -30,8 +24,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import glob
-
-
+from imutils.object_detection import non_max_suppression
 
 
 #build blob detector
@@ -62,14 +55,124 @@ ap.add_argument("-t", "--tracker", type=str, default="kcf",
 	help="OpenCV object tracker type")
 args = vars(ap.parse_args())
 
-img = cv2.imread('text.png',0)
-edges = cv2.Canny(img,100,200)
+img = cv2.imread('text.png', cv2.IMREAD_GRAYSCALE)
+edges = cv2.Canny(img, 100,200)
+
+
+width = 320
+height = 320
+
+orig = img.copy()
+(H, W) = img.shape[:2]
+
+
+newW = 320
+newH = 320
+rW = W / float(newW)
+rH = H / float(newH)
+
+# resize the image and grab the new image dimensions
+image = cv2.resize(img, (newW, newH))
+(H, W) = img.shape[:2]
+# define the two output layer names for the EAST detector model that
+# we are interested -- the first is the output probabilities and the
+# second can be used to derive the bounding box coordinates of text
+layerNames = [
+	"feature_fusion/Conv_7/Sigmoid",
+	"feature_fusion/concat_3"]
+
+# load the pre-trained EAST text detector
+print("[INFO] loading EAST text detector...")
+net = cv2.dnn.readNet(args["east"])
+
+# construct a blob from the image and then perform a forward pass of
+# the model to obtain the two output layer sets
+blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+	(123.68, 116.78, 103.94), swapRB=True, crop=False)
+start = time.time()
+net.setInput(blob)
+(scores, geometry) = net.forward(layerNames)
+end = time.time()
+
+# show timing information on text prediction
+print("[INFO] text detection took {:.6f} seconds".format(end - start))
+
+# grab the number of rows and columns from the scores volume, then
+# initialize our set of bounding box rectangles and corresponding
+# confidence scores
+(numRows, numCols) = scores.shape[2:4]
+rects = []
+confidences = []
+
+# loop over the number of rows
+for y in range(0, numRows):
+	# extract the scores (probabilities), followed by the geometrical
+	# data used to derive potential bounding box coordinates that
+	# surround text
+	scoresData = scores[0, 0, y]
+	xData0 = geometry[0, 0, y]
+	xData1 = geometry[0, 1, y]
+	xData2 = geometry[0, 2, y]
+	xData3 = geometry[0, 3, y]
+	anglesData = geometry[0, 4, y]
+	# loop over the number of columns
+	for x in range(0, numCols):
+		# if our score does not have sufficient probability, ignore it
+		if scoresData[x] < args["min_confidence"]:
+			continue
+
+		# compute the offset factor as our resulting feature maps will
+		# be 4x smaller than the input image
+		(offsetX, offsetY) = (x * 4.0, y * 4.0)
+
+		# extract the rotation angle for the prediction and then
+		# compute the sin and cosine
+		angle = anglesData[x]
+		cos = np.cos(angle)
+		sin = np.sin(angle)
+
+		# use the geometry volume to derive the width and height of
+		# the bounding box
+		h = xData0[x] + xData2[x]
+		w = xData1[x] + xData3[x]
+
+		# compute both the starting and ending (x, y)-coordinates for
+		# the text prediction bounding box
+		endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+		endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+		startX = int(endX - w)
+		startY = int(endY - h)
+
+		# add the bounding box coordinates and probability score to
+		# our respective lists
+		rects.append((startX, startY, endX, endY))
+		confidences.append(scoresData[x])
+
+# apply non-maxima suppression to suppress weak, overlapping bounding
+# boxes
+boxes = non_max_suppression(np.array(rects), probs=confidences)
+
+# loop over the bounding boxes
+for (startX, startY, endX, endY) in boxes:
+	# scale the bounding box coordinates based on the respective
+	# ratios
+	startX = int(startX * rW)
+	startY = int(startY * rH)
+	endX = int(endX * rW)
+	endY = int(endY * rH)
+
+	# draw the bounding box on the image
+	cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
+
+# show the output image
+cv2.imshow("Text Detection", orig)
+cv2.waitKey(0)
 
 
 
 
 # Detect blobs.
-keypoints = detector.detect(edges)
+keypoints = detector.detect(img)
 print(str(keypoints))
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
@@ -84,12 +187,13 @@ keypoints = detector.detect(edges)
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
 # the size of the circle corresponds to the size of blob
-im_with_keypoints = cv2.drawKeypoints(edges, keypoints, np.array([]), (200,0,200), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (200,0,200), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 cv2.imshow("Frame", im_with_keypoints)
 
 key = cv2.waitKey(0) & 0xFF
-
+# close all windows
+cv2.destroyAllWindows()
 
 # initialize OpenCV's special multi-object tracker
 trackers = cv2.MultiTracker_create()
