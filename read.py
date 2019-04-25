360082e659a3438d96ddf0c3e1885f6203ee152a
Dale Auten <dauten96@gmail.com>
2019-04-24 23:05:37 -0500

    manual selection withcharacter tracking is probably way to go

--- read.py
+++ read.py
@@ -27,196 +27,21 @@ import glob
 from imutils.object_detection import non_max_suppression
 
 
-#build blob detector
-params = cv2.SimpleBlobDetector_Params()
+# initialize OpenCV's special multi-object tracker
 
-# Change thresholds
-params.minThreshold = 0
-params.maxThreshold = 500
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(1.0)
 
-
-#Basically we don't care how big the blob is or how circular it is.
-#Any collection of pixels is a potential character
-params.filterByArea = False
-params.filterByConvexity = False
-params.filterByCircularity = False
-
-# Create a detector with the parameters
-detector = cv2.SimpleBlobDetector_create(params)
-
-
-
-
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", type=str,
-	help="path to input video file")
-ap.add_argument("-t", "--tracker", type=str, default="kcf",
-	help="OpenCV object tracker type")
-args = vars(ap.parse_args())
-
-img = cv2.imread('text.png', cv2.IMREAD_GRAYSCALE)
-edges = cv2.Canny(img, 100,200)
-
-
-width = 320
-height = 320
-
-orig = img.copy()
-(H, W) = img.shape[:2]
-
-
-newW = 320
-newH = 320
-rW = W / float(newW)
-rH = H / float(newH)
-
-# resize the image and grab the new image dimensions
-image = cv2.resize(img, (newW, newH))
-(H, W) = img.shape[:2]
-# define the two output layer names for the EAST detector model that
-# we are interested -- the first is the output probabilities and the
-# second can be used to derive the bounding box coordinates of text
-layerNames = [
-	"feature_fusion/Conv_7/Sigmoid",
-	"feature_fusion/concat_3"]
-
-# load the pre-trained EAST text detector
-print("[INFO] loading EAST text detector...")
-net = cv2.dnn.readNet(args["east"])
-
-# construct a blob from the image and then perform a forward pass of
-# the model to obtain the two output layer sets
-blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
-	(123.68, 116.78, 103.94), swapRB=True, crop=False)
-start = time.time()
-net.setInput(blob)
-(scores, geometry) = net.forward(layerNames)
-end = time.time()
-
-# show timing information on text prediction
-print("[INFO] text detection took {:.6f} seconds".format(end - start))
-
-# grab the number of rows and columns from the scores volume, then
-# initialize our set of bounding box rectangles and corresponding
-# confidence scores
-(numRows, numCols) = scores.shape[2:4]
-rects = []
-confidences = []
-
-# loop over the number of rows
-for y in range(0, numRows):
-	# extract the scores (probabilities), followed by the geometrical
-	# data used to derive potential bounding box coordinates that
-	# surround text
-	scoresData = scores[0, 0, y]
-	xData0 = geometry[0, 0, y]
-	xData1 = geometry[0, 1, y]
-	xData2 = geometry[0, 2, y]
-	xData3 = geometry[0, 3, y]
-	anglesData = geometry[0, 4, y]
-	# loop over the number of columns
-	for x in range(0, numCols):
-		# if our score does not have sufficient probability, ignore it
-		if scoresData[x] < args["min_confidence"]:
-			continue
-
-		# compute the offset factor as our resulting feature maps will
-		# be 4x smaller than the input image
-		(offsetX, offsetY) = (x * 4.0, y * 4.0)
-
-		# extract the rotation angle for the prediction and then
-		# compute the sin and cosine
-		angle = anglesData[x]
-		cos = np.cos(angle)
-		sin = np.sin(angle)
-
-		# use the geometry volume to derive the width and height of
-		# the bounding box
-		h = xData0[x] + xData2[x]
-		w = xData1[x] + xData3[x]
-
-		# compute both the starting and ending (x, y)-coordinates for
-		# the text prediction bounding box
-		endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
-		endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
-		startX = int(endX - w)
-		startY = int(endY - h)
-
-		# add the bounding box coordinates and probability score to
-		# our respective lists
-		rects.append((startX, startY, endX, endY))
-		confidences.append(scoresData[x])
-
-# apply non-maxima suppression to suppress weak, overlapping bounding
-# boxes
-boxes = non_max_suppression(np.array(rects), probs=confidences)
-
-# loop over the bounding boxes
-for (startX, startY, endX, endY) in boxes:
-	# scale the bounding box coordinates based on the respective
-	# ratios
-	startX = int(startX * rW)
-	startY = int(startY * rH)
-	endX = int(endX * rW)
-	endY = int(endY * rH)
-
-	# draw the bounding box on the image
-	cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
-
-# show the output image
-cv2.imshow("Text Detection", orig)
-cv2.waitKey(0)
-
-
-
-
-# Detect blobs.
-keypoints = detector.detect(img)
-print(str(keypoints))
-# Draw detected blobs as red circles.
-# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-# the size of the circle corresponds to the size of blob
-im_with_keypoints = cv2.drawKeypoints(edges, keypoints, np.array([]), (200,0,200), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-cv2.imshow("Frame", im_with_keypoints)
-# Detect blobs.
-keypoints = detector.detect(edges)
-
-
-# Draw detected blobs as red circles.
-# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-# the size of the circle corresponds to the size of blob
-im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (200,0,200), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-cv2.imshow("Frame", im_with_keypoints)
-
-key = cv2.waitKey(0) & 0xFF
-# close all windows
-cv2.destroyAllWindows()
 
 # initialize OpenCV's special multi-object tracker
 trackers = cv2.MultiTracker_create()
-
-# if a video path was not supplied, grab the reference to the web cam
-if not args.get("video", False):
-	print("[INFO] starting video stream...")
-	vs = VideoStream(src=0).start()
-	time.sleep(1.0)
-
-# otherwise, grab a reference to the video file
-else:
-	vs = cv2.VideoCapture(args["video"])
-
-files = []
 
 # loop over frames from the video stream
 while True:
 	# grab the current frame, then handle if we are using a
 	# VideoStream or VideoCapture object
 	frame = vs.read()
-	frame = frame[1] if args.get("video", False) else frame
-
 
 	# check to see if we have reached the end of the stream
 	if frame is None:
@@ -224,30 +49,30 @@ while True:
 
 	# resize the frame (so we can process it faster)
 	frame = imutils.resize(frame, width=600)
-	edges = cv2.Canny(frame,100,200)
 
-	# Detect blobs.
-	keypoints = detector.detect(edges)
+	(success, boxes) = trackers.update(frame)
+	# loop over the bounding boxes and draw then on the frame
+	for n, box in enumerate(boxes):
+		(x, y, w, h) = [int(v) for v in box]
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-	# Draw detected blobs as red circles.
-	# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-	# the size of the circle corresponds to the size of blob
-	im_with_keypoints = cv2.drawKeypoints(edges, keypoints, np.array([]), (200,0,200), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-	cv2.imshow("Frame", im_with_keypoints)
+	cv2.imshow("Frame", frame)
 
 	key = cv2.waitKey(1) & 0xFF
+	if key == ord("s"):
 
+		# select the bounding box of the object we want to track (make
+		# sure you press ENTER or SPACE after selecting the ROI)
+		box = cv2.selectROI("Frame", frame, fromCenter=False,
+			showCrosshair=True)
 
+		# create a new object tracker for the bounding box and add it
+		# to our multi-object tracker
 
+		trackers.add(cv2.TrackerCSRT_create() , frame, box)
 
-# if we are using a webcam, release the pointer
-if not args.get("video", False):
-	vs.stop()
+vs.stop()
 
-# otherwise, release the file pointer
-else:
-	vs.release()
 
 # close all windows
 cv2.destroyAllWindows()
